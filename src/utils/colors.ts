2f23ba191faaa727d47bc7f040cf0ef367bae09c
BlankKeyboard <50400175+BlankKeyboard@users.noreply.github.com>
2025-05-20 03:58:43 +1200

    initial commit 24

---
+++ src/utils/colors.ts
@@ -0,0 +1,29 @@
+type Painter = (value: string) => string;
+
+const code = (open: number, close = 39): Painter => {
+  return (value: string) => `\x1b[${open}m${value}\x1b[${close}m`;
+};
+
+export const colors = {
+  danger: code(31),
+  goblin: code(32),
+  info: code(36),
+  mystical: code(35),
+  success: code(32),
+  treasure: code(33),
+  warning: code(33)
+};
+
+export const symbols = {
+  check: "[ok]",
+  coin: "[gold]",
+  gem: "[gem]",
+  goblin: "[goblin]",
+  lair: "[lair]",
+  quest: "[quest]",
+  scroll: "[scroll]",
+  sparkles: "[spark]",
+  spell: "[spell]",
+  sword: "[str]",
+  treasure: "[treasure]"
+};
